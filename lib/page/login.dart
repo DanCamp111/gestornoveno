e4fc8731d176afba101369ac87b9764a74b1d036
DanCamp111 <dan90002014@gmail.com>
2024-09-25 16:47:02 -0600

    Se agrega el uso de la api de login

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:proyecto/Models/LoginResponse.dart';
+import 'package:proyecto/page/home.dart';
 import 'package:quickalert/quickalert.dart';
+import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -9,6 +15,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController txtUser = TextEditingController();
+  TextEditingController txtPass = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,26 +32,46 @@ class _LoginState extends State<Login> {
             Padding(
                 padding: EdgeInsets.all(40),
                 child: TextFormField(
-                  decoration: InputDecoration(label: Text('Usuario')
-                  ),
-                )
-            ),
+                  controller: txtUser,
+                  decoration: InputDecoration(label: Text('Usuario')),
+                )),
             Padding(
               padding: EdgeInsets.fromLTRB(40, 0, 40, 40),
               child: TextFormField(
-                decoration: InputDecoration(label: Text('Contraseña')
-                ),
+                controller: txtPass,
+                decoration: InputDecoration(label: Text('Contraseña')),
                 obscureText: true,
               ),
             ),
-            TextButton(onPressed: ()
-            {
-              QuickAlert.show(
-                context: context,
-                type: QuickAlertType.success,
-                text: 'Transaction Completed Successfully!',
-              );
-            }, child: Text('Acceder'))
+            TextButton(
+                onPressed: () async {
+                  final response = await http.post(
+                      Uri.parse('http://10.172.254.247:8000/api/login'),
+                      body: jsonEncode(<String, dynamic>{
+                        'email': txtUser.text,
+                        'password': txtPass.text
+                      }),
+                      headers: <String, String>{
+                        'Content-Type': 'application/json; charset=UTF-8'
+                      });
+
+                  print(response.body);
+                  Map<String, dynamic> responseJson = jsonDecode(response.body);
+                  final loginResponse = LoginResponse.fromJson(responseJson);
+                  if (loginResponse.acceso == "OK") {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => const Home())
+                  );
+                  } else {
+                    QuickAlert.show(
+                      context: context,
+                      type: QuickAlertType.error,
+                      title: 'Oops...',
+                      text: 'Sorry, something went wrong',
+                    );
+                  }
+                },
+                child: Text('Acceder'))
           ],
         ),
       ),
