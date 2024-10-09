6c4161bd819e7c72813a7f3286636d9931e5853b
DanCamp111 <dan90002014@gmail.com>
2024-10-09 16:30:42 -0600

    Se agrego la funcion de agregar categorias nuevas y mostrarlas en el home

---
+++ lib/page/NuevaCategoria.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:proyecto/page/home.dart';
+
+class Nuevacategoria extends StatefulWidget {
+  const Nuevacategoria({super.key});
+
+  @override
+  State<Nuevacategoria> createState() => _NuevacategoriaState();
+}
+
+class _NuevacategoriaState extends State<Nuevacategoria> {
+  TextEditingController txtNombre = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Categorias"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(40),
+              child: TextFormField(
+                controller: txtNombre,
+                decoration: InputDecoration(label: Text('Usuario')),
+              ),
+            ),
+            TextButton(
+              onPressed: () async {
+                final response = await http.post(
+                  Uri.parse('http://10.172.254.247:8000/api/categorias/save'),
+                  body: jsonEncode(<String, dynamic>{
+                    'nombre': txtNombre.text,
+                    'id_usuario': 1
+                  }),
+                  headers: <String, String>{
+                    'Content-Type': 'application/json; charset=UTF-8'
+                  },
+                );
+
+                print(response.body);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Home()),
+                ); // Cierre del Navigator.push
+              },
+              child: Text("Guardar"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:proyecto/Models/Categorias.dart';
+import 'package:http/http.dart' as http;
+import 'package:proyecto/page/NuevaCategoria.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +13,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Categorias> categorias = [];
+
+  void fnObtenerCategorias() async {
+    final response = await http.get(
+        Uri.parse('http://10.172.254.247:8000/api/categorias'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Accept': 'application/json'
+        });
+    print(response.body);
+
+    Iterable mapCategorias = jsonDecode(response.body);
+    categorias = List<Categorias>.from(
+        mapCategorias.map((model) => Categorias.fromJson(model)));
+    setState(() {});
+  }
+
+  Widget _ListviewCategorias() {
+    return ListView.builder(
+      itemCount: categorias.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(categorias[index].id.toString()),
+          subtitle: Text(categorias[index].nombre),
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fnObtenerCategorias();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Categorias"),
+      ),
+      body: _ListviewCategorias(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+         Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => const Nuevacategoria())
+        );
+      },child: Icon(Icons.add),),
+    );
   }
 }
